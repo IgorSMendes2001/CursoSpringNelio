33a93b148a63cb293ccb097c8afc99789d0d8005
IgorSMendes2001 <igor07082001@gmail.com>
2022-09-01 14:23:32 -0300

    Arquivo inicial do Spring com as dependências

---
+++ src/main/java/com/devsuperior/cursospring/CursospringApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.cursospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursospringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsuperior/cursospring/CursospringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.cursospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
