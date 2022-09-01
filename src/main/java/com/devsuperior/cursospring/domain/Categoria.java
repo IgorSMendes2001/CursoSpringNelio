1f6463a26046b6a83a7041427d9cb58f702fe141
IgorSMendes2001 <igor07082001@gmail.com>
2022-09-01 16:20:20 -0300

    Adicionando Banco de dados e Mapeamento

--- src/main/java/com/devsuperior/cursospring/domain/Categoria.java
+++ src/main/java/com/devsuperior/cursospring/domain/Categoria.java
@@ -1,9 +1,14 @@
 package com.devsuperior.cursospring.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
-
+@Entity
 public class Categoria implements Serializable {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
 
