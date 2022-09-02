2833c3c8c0a80e16849c486afa627313cbb16fcb
IgorSMendes2001 <igor07082001@gmail.com>
2022-09-02 11:13:19 -0300

    Criando instanciação

--- src/main/java/com/devsuperior/cursospring/CursospringApplication.java
+++ src/main/java/com/devsuperior/cursospring/CursospringApplication.java
@@ -1,13 +1,28 @@
 package com.devsuperior.cursospring;
 
+import com.devsuperior.cursospring.domain.Categoria;
+import com.devsuperior.cursospring.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class CursospringApplication {
+public class CursospringApplication implements CommandLineRunner {
+	@Autowired
+	private CategoriaRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1=new Categoria(null,"Informática");
+		Categoria cat2= new Categoria(null,"Escritório");
+		repository.saveAll(Arrays.asList(cat1,cat2));
+
+	}
 }
